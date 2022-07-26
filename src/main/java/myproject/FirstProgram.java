9a9aa6f6030bb7f410127f17db28ee549e1daf9a
Valeriia Kucheriava <Valeriia_Kucheriava@epam.com>
2022-07-26 21:08:37 +0200

    initial commit

---
+++ src/main/java/myproject/FirstProgram.java
@@ -0,0 +1,11 @@
+package myproject;/* Here
+is may first program 
+*/
+
+class FirstProgram{
+
+//it's a comment
+public static void main(String[] args){
+	System.out.println("Hello, world");
+	}
+}
